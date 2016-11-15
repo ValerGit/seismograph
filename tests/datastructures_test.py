6d31a82667c0eebae5b19e5fde0af251e483f6b6
SchadkoAO <schadko.ao@gmail.com>
2016-11-15 20:14:20 +0300

    __setattr__ and __getattr__

--- tests/datastructures_test.py
+++ tests/datastructures_test.py
@@ -7,8 +7,27 @@ from seismograph.datastructures import Context
 class DataStructuresTestCase(unittest.TestCase):
     def setUp(self):
         self.context = Context()
-        self.testItem = 0
+        self.testItem = "TEST"
+
+    def test_getattr_AttributeError_message(self):
+        errorString = '"Context" does not have "{}" attribute.'.format(self.testItem)
+        try:
+            self.context.__delattr__(self.testItem)
+        except AttributeError as error:
+            self.assertEquals(error.message, errorString)
+
+    def test_getattr_raise_AttributeError(self):
+        self.assertRaises(AttributeError, self.context.__getattr__, self.testItem)
+
+    def test_setattr(self):
+        testValue = "hello world"
+        self.context.__setattr__(self.testItem, testValue)
+        self.assertEqual(self.context[self.testItem], testValue)
 
 
     def tearDown(self):
         pass
+
+
+if __name__ == '__main__':
+    unittest.main()
